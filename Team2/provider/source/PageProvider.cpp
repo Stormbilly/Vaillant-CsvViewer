ee45a51342cf25120a22be347cec66194d445fb8
Matthias Wellhoefer <matthias.wellhoefer@vaillant-group.com>
2023-08-08 12:11:02 +0200

    PageProvider Implementation
    PageProvider implemented
    GoogleTests added

--- Team2/provider/source/PageProvider.cpp
+++ Team2/provider/source/PageProvider.cpp
@@ -4,7 +4,14 @@ namespace csv
 {
     contracts::Page PageProvider::ProvidePage(const contracts::Page& csv_data, int page_number, int lines_per_page)
     {
-        return contracts::Page();
-    }
+        contracts::Page page;
+        int startindex = (page_number - 1) * lines_per_page; //Correction with -1 to start at location 0
 
+        page.Header = csv_data.Header;
+        for(int i=0; i < lines_per_page; i++)
+        {
+            page.Rows.push_back(csv_data.Rows[startindex + i]);
+        }
+        return page;
+    }
 } // namespace csv
---
+++ Team2/provider/tests/PageProvider.cpp
@@ -0,0 +1,80 @@
+#include <gmock/gmock.h>
+#include "provider/source/PageProvider.h"
+
+namespace csv
+{
+        TEST(PageProviderTests, ReturnPage_ReturnPage1Entries3)
+        {
+        contracts::Page csv_data{{"Name", "Street", "PLZ"},
+                                        {{"A", "S1", "1"},
+                                        {"B", "S2", "2"},
+                                        {"C", "S3", "3"},
+                                        {"D", "S4", "4"},
+                                        {"E", "S5", "5"},
+                                        {"F", "S6", "6"},
+                                        {"G", "S7", "7"},
+                                        {"H", "S8", "8"},
+                                        {"I", "S8", "9"},
+                                        {"J", "S8", "10"}}};
+        contracts::Page expected_page{{"Name", "Street", "PLZ"},
+                                        {{"A", "S1", "1"},
+                                        {"B", "S2", "2"},
+                                        {"C", "S3", "3"}}};   
+        PageProvider page_provider;
+        contracts::Page result_page;
+        result_page = page_provider.ProvidePage(csv_data, 1, 3);
+
+        EXPECT_EQ(csv_data.Header, result_page.Header);
+        EXPECT_EQ(expected_page.Rows[0], result_page.Rows[0]);
+        EXPECT_EQ(expected_page.Rows[1], result_page.Rows[1]);
+        EXPECT_EQ(expected_page.Rows[2], result_page.Rows[2]);
+        }
+
+        TEST(PageProviderTests, ReturnPage_ReturnPage2Entries2)
+        {
+        contracts::Page csv_data{{"Name", "Street", "PLZ"},
+                                        {{"A", "S1", "1"},
+                                        {"B", "S2", "2"},
+                                        {"C", "S3", "3"},
+                                        {"D", "S4", "4"},
+                                        {"E", "S5", "5"},
+                                        {"F", "S6", "6"},
+                                        {"G", "S7", "7"},
+                                        {"H", "S8", "8"},
+                                        {"I", "S8", "9"},
+                                        {"J", "S8", "10"}}};
+        contracts::Page expected_page{{"Name", "Street", "PLZ"},
+                                        {{"C", "S3", "3"},
+                                        {"D", "S4", "4"}}};   
+        PageProvider page_provider;
+        contracts::Page result_page;
+        result_page = page_provider.ProvidePage(csv_data, 2, 2);
+
+        EXPECT_EQ(csv_data.Header, result_page.Header);
+        EXPECT_EQ(expected_page.Rows[0], result_page.Rows[0]);
+        EXPECT_EQ(expected_page.Rows[1], result_page.Rows[1]);
+        }
+
+        TEST(PageProviderTests, ReturnPage_ReturnPage8Entries1)
+        {
+        contracts::Page csv_data{{"Name", "Street", "PLZ"},
+                                        {{"A", "S1", "1"},
+                                        {"B", "S2", "2"},
+                                        {"C", "S3", "3"},
+                                        {"D", "S4", "4"},
+                                        {"E", "S5", "5"},
+                                        {"F", "S6", "6"},
+                                        {"G", "S7", "7"},
+                                        {"H", "S8", "8"},
+                                        {"I", "S8", "9"},
+                                        {"J", "S8", "10"}}};
+        contracts::Page expected_page{{"Name", "Street", "PLZ"},
+                                        {{"H", "S8", "8"}}};   
+        PageProvider page_provider;
+        contracts::Page result_page;
+        result_page = page_provider.ProvidePage(csv_data, 8, 1);
+
+        EXPECT_EQ(csv_data.Header, result_page.Header);
+        EXPECT_EQ(expected_page.Rows[0], result_page.Rows[0]);
+        }
+}
