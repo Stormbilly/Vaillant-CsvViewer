23a520dc0e9fbf9617f9d7730132c45fed96e3f5
DavidKniss <david.kniss@vaillant-group.com>
2023-08-08 12:12:47 +0200

    remove page tests from domain tests

--- Team1/domain/tests/Domain.Tests.cpp
+++ Team1/domain/tests/Domain.Tests.cpp
@@ -12,41 +12,5 @@ namespace csv
 		{
 			Domain domain;
 		}
-
-		TEST(DomainTests, Page_Constructor_EmptyCsv)
-		{
-			std::vector <std::vector <std::string> > csv;
-
-			contracts::Page dummy_page(csv, 42);
-
-			EXPECT_THAT(dummy_page.header.size(), 0);
-			EXPECT_THAT(dummy_page.values.size(), 0);
-			EXPECT_THAT(dummy_page.firstLine, 0);
-			EXPECT_THAT(dummy_page.numberOfRows, 42);
-		}
-
-		TEST(DomainTests, Page_Constructor_InvalidNumberOfRows)
-		{
-			std::vector <std::vector <std::string> > csv = {{"header1", "header2"}};
-
-			contracts::Page dummy_page(csv, 42);
-
-			EXPECT_THAT(dummy_page.header.size(), 0);
-			EXPECT_THAT(dummy_page.values.size(), 0);
-			EXPECT_THAT(dummy_page.firstLine, 0);
-			EXPECT_THAT(dummy_page.numberOfRows, 42);
-		}
-
-		TEST(DomainTests, Page_Constructor_ValidNumberOfRows)
-		{
-			std::vector <std::vector <std::string> > csv = {{"header1", "header2"}, {"value1", "value2"}};
-
-			contracts::Page dummy_page(csv, 24);
-
-			EXPECT_THAT(dummy_page.header.size(), 1);
-			EXPECT_THAT(dummy_page.values.size(), 1);
-			EXPECT_THAT(dummy_page.firstLine, 0);
-			EXPECT_THAT(dummy_page.numberOfRows, 24);
-		}
 	}
 }
