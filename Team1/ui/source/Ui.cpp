ec5b3dafc2a6ed1ee2a9c765b7b9cac4636117ef
Your Name <you@example.com>
2023-08-08 15:43:07 +0200

    UI

--- Team1/ui/source/Ui.cpp
+++ Team1/ui/source/Ui.cpp
@@ -1,6 +1,36 @@
+#include <vector>
+#include <iostream>
 #include "Ui.h"
 
 namespace csv
 {
 	
+    std::string BuildOutputLine(const std::vector<std::string>& strings);
+
+    void ShowPage(const contracts::Page& page) {
+        ::system("clear");
+
+        std::cout  << BuildOutputLine(page.header).data();
+        std::cout << "--------------------------------------\n";
+        
+        
+        for (int i = page.firstLine; i < page.firstLine + page.numberOfRows; i++) {
+//        for (auto itVec = page.values[page.firstLine]; itVec != page.values[page.firstLine + page.numberOfRows]; itVec++) { 
+                std::cout << BuildOutputLine(page.values[i]).data();
+        }
+        std::cout << "--------------------------------------\n";
+        std::cout << "F)irst page, P)revious page, N)ext page, Last page, E)xit\n";
+
+    } 
+
+    std::string BuildOutputLine(const std::vector<std::string>& strings) {
+        
+        std::string output;
+        
+        for (auto itVec = strings.begin(); itVec != strings.end(); itVec++) {
+            output.append(*itVec +  "|");
+        }; 
+        output.append("\n");
+        return output;
+    }           
 } // namespace csv
--- Team1/ui/source/Ui.h
+++ Team1/ui/source/Ui.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "Page.h"
+
 namespace csv
 {
 	struct Ui
 	{
 	};
+
+	void ShowPage(const contracts::Page& page);
 	
 } // namespace csv
