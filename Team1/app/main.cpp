ec3d7223b8950d6651a39f9cf0777139419197fc
DavidKniss <david.kniss@vaillant-group.com>
2023-08-21 16:41:08 +0200

    main: add user input read

--- Team1/app/main.cpp
+++ Team1/app/main.cpp
@@ -1,12 +1,40 @@
 ﻿#include "Interactor.h"
 #include "Ui.h"
+#include <iostream>
 
 using namespace csv;
 
 int main(int argc, const char** argv)
 {
-    const auto page = Interactor::Start(argv, argc);
-    ShowPage(page);
+    std::string user_input;
+    bool errorFlag = false;
+    auto page = Interactor::Start(argv, argc);
+
+    while(true)
+    {
+        ShowPage(page);
+
+        if(errorFlag)
+        {
+            std::cout << "Invalid command" << std::endl;
+            errorFlag = false;
+        }
+
+        std::cin >> user_input;
+
+        if(user_input == "F")
+            page = Interactor::NextPage(page);
+        else if(user_input == "P")
+            page = Interactor::PrevPage(page);
+        else if(user_input == "N")
+            page = Interactor::NextPage(page);
+        //else if(user_input == "L")
+        //    page = Interactor::LastPage(page);
+        else if(user_input == "E")
+            exit(0);
+        else
+            errorFlag = true;
+    }
 
 	return 0;
 }
