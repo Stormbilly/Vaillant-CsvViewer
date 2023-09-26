be9b4cbb8d7ed56b7a953de7badc18a9c76086d1
Matthias Wellhoefer <matthias.wellhoefer@vaillant-group.com>
2023-09-26 12:41:05 +0200

    CSVFileParser implemented
    - Implementation done
    - Unittest done

--- Team2/domain/source/CSVFileParser.cpp
+++ Team2/domain/source/CSVFileParser.cpp
@@ -2,30 +2,31 @@
 
 namespace csv
 {
-    contracts::Page CSVFileParser::ParseCSV(const std::vector<std::string>& csv_file_data)
-    {
-        auto page = contracts::Page{};
-        int header_column = 0;
+    const std::string delimiter = ";";
 
-        for (auto it = csv_file_data[0].begin(); it != csv_file_data[0].end(); it ++)
-        {
-            if (*it == ';'){
-                header_column ++;
-            }
-            else if(*it == '/n'){
-                break;
-            }
-            else{
-                page.Header[header_column] += *it;
+    std::vector<std::string> splitLine(std::string line) {
+        std::vector<std::string> vec;
+        size_t pos_start = 0;
+        size_t pos_end = 0;
 
-            }
+        while ((pos_end = line.find(delimiter, pos_start)) != std::string::npos) {
+            vec.push_back(line.substr(pos_start, pos_end - pos_start));
+            pos_start = pos_end + 1; //Ignore found delimiter
         }
-
-        // for (auto it = csv_file_data[1].begin() ;  csv_file_data) {
-
-        // }
-
-        return page;
+        //Don´t forget last element
+        vec.push_back(line.substr(pos_start, line.size() - pos_start));
+        return vec;
     }
 
+    contracts::Page CSVFileParser::ParseCSV(const std::vector<std::string>& csv_file_data)
+    {
+        contracts::Page parsedCSV;
+
+        parsedCSV.Header = splitLine(csv_file_data[0]);
+        for(int i=1; i<csv_file_data.size(); i++) {
+            parsedCSV.Rows.push_back(splitLine(csv_file_data[i]));
+        }
+
+        return parsedCSV;
+    }
 } // namespace csv
