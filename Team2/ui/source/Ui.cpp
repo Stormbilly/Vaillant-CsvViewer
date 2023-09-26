cf7c4c1c69f080db39b2b955c7348d29e362a11a
holger-schoenauer <holger.schoenauer@vaillant-group.com>
2023-09-26 13:46:49 +0200

    UI implemented

--- Team2/ui/source/Ui.cpp
+++ Team2/ui/source/Ui.cpp
@@ -1,9 +1,31 @@
 #include "Ui.h"
-
+#include <iostream>
+#include <iomanip>
 namespace csv
 {
     void Ui::ShowPage(const contracts::Page& page)
     {
+        //print header
+        for(auto& elem : page.Header)
+        {
+            std::cout << elem << " " << "|";
+        }
+        std::cout << std::endl;
+
+        for(auto elem : page.Header)
+        {
+            std::cout << std::setfill('-') << std::setw(elem.size() + 2) << "+";
+        }
+        std::cout << std::endl;
+
+        for(auto& row : page.Rows)
+        {
+            for(auto& elem: row)
+            {
+                std::cout << elem << " " << "|";
+            }
+            std::cout << std::endl;
+        }
     }
 
 } // namespace csv
